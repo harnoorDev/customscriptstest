366649b545af3d656b7be741a07706ad17f5581b
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-07 14:55:56 -0400

    Update TurnOffFirewall.ps1

--- TurnOffFirewall.ps1
+++ TurnOffFirewall.ps1
@@ -10,3 +10,10 @@ Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Par
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
 
 Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0
+
+#Showing All System Tray Icons
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
+
+:: To kill and restart explorer
+taskkill /f /im explorer.exe
+start explorer.exe
