0acc0d8bb11d2b0d4f7352887606746ae7e9a637
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-07 04:31:21 -0400

    Added Firewall and Installer scripts

---
+++ DownloadInstallMSI.ps1
@@ -0,0 +1,23 @@
+#Download and Run MSI package for Automated install
+$uri = "https://openvpn.net/downloads/openvpn-connect-v3-windows.msi"
+$uri2 = "https://download.microsoft.com/download/C/B/3/CB35F695-5A32-4458-ACDB-E701250CEA1E/SQLDataSyncAgent-2.0-x64-ENU.msi"
+$out = "C:\OpenVPNInstaller.msi"
+$out2 = "C:\SQLDataSync.msi"
+
+Function Download_Installers{
+Invoke-WebRequest -uri $uri -OutFile $out
+Invoke-WebRequest -uri $uri2 -OutFile $out2
+$msifile = Get-ChildItem -Path $out -File -Filter '*.ms*' 
+$msifile2 = Get-ChildItem -Path $out2 -File -Filter '*.ms*' 
+
+}
+
+Function Install_OpenVPN{
+msiexec /i $out /passive
+
+}
+
+Download_Installers
+Install_OpenVPN
+
+
---
+++ TurnOffFirewall.ps1
@@ -0,0 +1,12 @@
+
+
+Set-TimeZone -Id "Eastern Standard Time" -PassThru
+
+netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow 
+
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -name "EnableFirewall" -Value 0
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
+
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0
