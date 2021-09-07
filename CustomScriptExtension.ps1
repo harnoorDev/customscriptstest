b71a2edcc463d4101a660de2fa0c9fba68aab651
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-07 04:30:51 -0400

    Update CustomScriptExtension.ps1

--- CustomScriptExtension.ps1
+++ CustomScriptExtension.ps1
@@ -1,16 +1,5 @@
 
 
-Set-TimeZone -Id "Eastern Standard Time" -PassThru
-
-netsh advfirewall firewall add rule name="ICMP Allow incoming V4 echo request" protocol=icmpv4:8,any dir=in action=allow 
-
-
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\DomainProfile' -name "EnableFirewall" -Value 0
-
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\PublicProfile' -name "EnableFirewall" -Value 0
-
-Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\services\SharedAccess\Parameters\FirewallPolicy\Standardprofile' -name "EnableFirewall" -Value 0
-
 ## Variables
  
 $bgInfoFolder = "C:\BgInfo"
@@ -90,27 +79,4 @@ Start-Sleep 3
 stop-process -Id $PID
  
 ## ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
-#Download and Run MSI package for Automated install
-$uri = "https://openvpn.net/downloads/openvpn-connect-v3-windows.msi"
-$uri2 = "https://download.microsoft.com/download/C/B/3/CB35F695-5A32-4458-ACDB-E701250CEA1E/SQLDataSyncAgent-2.0-x64-ENU.msi"
-$out = "C:\OpenVPNInstaller.msi"
-$out2 = "C:\SQLDataSync.msi"
-
-Function Download_Installers{
-Invoke-WebRequest -uri $uri -OutFile $out
-Invoke-WebRequest -uri $uri2 -OutFile $out2
-$msifile = Get-ChildItem -Path $out -File -Filter '*.ms*' 
-$msifile2 = Get-ChildItem -Path $out2 -File -Filter '*.ms*' 
-
-}
-
-Function Install_OpenVPN{
-msiexec /i $out /passive
-
-}
-
-Download_Installers
-Install_OpenVPN
-
 
