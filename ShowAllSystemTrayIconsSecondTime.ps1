b2ff1624d7d23da1ab7de3514ab972c5a3826bf5
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-09 17:26:52 -0400

    Add files via upload

---
+++ ShowAllSystemTrayIconsSecondTime.ps1
@@ -0,0 +1,10 @@
+Set-ExecutionPolicy RemoteSigned -CurrentScope Process -Force
+Set-ExecutionPolicy RemoteSigned -CurrentScope CurrentUser -Force
+Set-ExecutionPolicy RemoteSigned -CurrentScope LocalMachine -Force
+
+#Showing All System Tray Icons
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
+
+
+taskkill /f /im explorer.exe
+start explorer.exe
