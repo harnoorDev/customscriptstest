a592d221df4fb440caca6bd1dcc38bd120a3fa99
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-13 09:22:13 -0400

    Update DownloadInstallMSI.ps1

--- DownloadInstallMSI.ps1
+++ DownloadInstallMSI.ps1
@@ -4,8 +4,10 @@ $uri2 = "https://download.microsoft.com/download/C/B/3/CB35F695-5A32-4458-ACDB-E
 $uri3 = "https://raw.githubusercontent.com/harnoorDev/customscriptstest/main/ShowAllSystemTrayIconsSecondTime.ps1"
 $out = "C:\OpenVPNInstaller.msi"
 $out2 = "C:\SQLDataSync.msi"
-$username = $env:UserName
-$out3 = "C:\Users\$username\Desktop\Show_All_Icons.ps1"
+#$username = $env:UserName
+#$out3 = "C:\Users\$username\Desktop\Show_All_Icons.ps1"
+$out3 = "C:\Show_All_Icons.ps1"
+
 
 Function Download_Installers{
 Invoke-WebRequest -uri $uri -OutFile $out
