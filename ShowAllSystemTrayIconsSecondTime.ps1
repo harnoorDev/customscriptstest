5079fd9dfb7eea8988349d23d1b4ad394b52dbbb
Harnoor Singh <45241855+harnoorDev@users.noreply.github.com>
2021-09-15 11:51:43 -0400

    Update ShowAllSystemTrayIconsSecondTime.ps1

--- ShowAllSystemTrayIconsSecondTime.ps1
+++ ShowAllSystemTrayIconsSecondTime.ps1
@@ -5,6 +5,7 @@ Set-ExecutionPolicy RemoteSigned -CurrentScope LocalMachine -Force
 #Showing All System Tray Icons
 REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F
 
+C:\BgInfo\Bginfo.exe C:\BgInfo\logon.bgi /timer:0 /nolicprompt /silent
 
 taskkill /f /im explorer.exe
 start explorer.exe
